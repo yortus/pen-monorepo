f04ea2c4f31caa73cd886518aa7421cfe64bb52c
Troy Gerwien <yortus@gmail.com>
2020-03-04 08:59:28 +0800

    temporarily disable codegen
    - pending changes to symbol resolution

--- src/compile.ts
+++ src/compile.ts
@@ -1,12 +1,12 @@
-import * as fs from 'fs';
-import * as mkdirp from 'mkdirp';
-import * as path from 'path';
+// import * as fs from 'fs';
+// import * as mkdirp from 'mkdirp';
+// import * as path from 'path';
 import {CompilerOptions} from './compiler-options';
 import {createSourceFileGraph} from './transforms';
 import {parseSourceFiles} from './transforms';
 import {createSymbolDefinitions} from './transforms';
 import {resolveSymbolReferences} from './transforms';
-import {generateTargetCode} from './transforms';
+// import {generateTargetCode} from './transforms';
 
 
 export function compile(compilerOptions: CompilerOptions) {
@@ -14,15 +14,19 @@ export function compile(compilerOptions: CompilerOptions) {
     let ast01 = parseSourceFiles(sourceFiles);
     let ast02 = createSymbolDefinitions(ast01);
     let ast03 = resolveSymbolReferences(ast02);
-    let ast04 = generateTargetCode(ast03);
+    // let ast04 = generateTargetCode(ast03);
 
-    let result = `// tslint:disable: all\n`;
-    for (let [, code] of ast04.entries()) {
-        result += code;
-    }
+    // let result = `// tslint:disable: all\n`;
+    // for (let [, code] of ast04.entries()) {
+    //     result += code;
+    // }
 
-    // write the target code to the output directory
-    let tempOutFilePath = path.join(compilerOptions.outDir, 'temp.ts');
-    mkdirp.sync(compilerOptions.outDir);
-    fs.writeFileSync(tempOutFilePath, result);
+    // // write the target code to the output directory
+    // let tempOutFilePath = path.join(compilerOptions.outDir, 'temp.ts');
+    // mkdirp.sync(compilerOptions.outDir);
+    // fs.writeFileSync(tempOutFilePath, result);
+
+
+    [] = [ast03];
+
 }
