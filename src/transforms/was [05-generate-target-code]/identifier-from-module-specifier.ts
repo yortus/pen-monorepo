2d17b08d0799396bb11c84458b1667f0e55ff363
Troy Gerwien <yortus@gmail.com>
2020-03-14 15:24:48 +0800

    remove unused code

--- src/transforms/was [05-generate-target-code]/identifier-from-module-specifier.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-
-
-export function identifierFromModuleSpecifier(modspec: string) {
-    // TODO: don't allow clashes with other compiler ids, eg __std
-    let id = path.basename(modspec);
-    id = id.replace(/[^a-zA-Z0-9]/g, '_');
-    return '__' + id;
-}
--- src/transforms/was [05-generate-target-code]/target-code.ts
+++
@@ -1,4 +0,0 @@
-import {SourceFile} from '../../ast-nodes';
-
-
-export type TargetCode = ReadonlyMap<SourceFile, string>;
