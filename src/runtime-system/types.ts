00799614b139989bff62c31229cde511b3f7cad4
Troy Gerwien <yortus@gmail.com>
2020-03-15 14:19:19 +0800

    update TODO

--- src/runtime-system/types.ts
+++ src/runtime-system/types.ts
@@ -1,22 +1,3 @@
-/* Bikeshed names:
-- relation (sounds like some abstract math concept - could be confusing)
-- rule
-- pattern (too generic?)
-- parser (one-sided - doesn't including unparsing)
-- production (proper term for this. But one-sided? nah seems ok actually - same term used in both generative grammars and recognizers
-- form
-- combinator
-- transducer
-- axiom
-- formula
-- model
-- shape
-- schema
-- definition
-*/
-
-
-
 /**
  * TODO: doc...
  * - modifies `result` iff return value is true -OR- if returns false, result may be garbage WHICH IS IT? 2nd is more flexible for impls
