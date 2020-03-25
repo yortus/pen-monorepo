f3d8f00069564da0a6b813086465032bfc8e3558
Troy Gerwien <yortus@gmail.com>
2020-03-25 10:40:59 +0800

    added memo to std

--- src/standard-library/memo.ts
+++ src/standard-library/memo.ts
@@ -1,49 +1,46 @@
 // TODO: investigate... need to use `text` as part of memo key? Study lifecycle/extent of each `memos` instance.
 function memo(expr: Production): Production {
 
-    // TODO: for parse...
-    const memos = new Map<number, {
+    const parseMemos = new Map<number, {
         resolved: boolean,
         isLeftRecursive: boolean,
         result: {node: unknown, posᐟ: number}
     }>();
 
-    // TODO: for unparse...
     // TODO: revise memo key once using new ast/pos signature
-    const memos = new Map<
+    const unparseMemos = new Map<
         unknown,
-        Map<
-            number,
-            {resolved: boolean, isLeftRecursive: boolean, result: {src: string, posᐟ: number}}
-        >
+        Map<number, {
+            resolved: boolean,
+            isLeftRecursive: boolean,
+            result: {text: string, posᐟ: number}
+        }>
     >();
-
-
 
     return {
         kind: 'production',
 
         parse(text, pos, result) {
             // Check whether the memo table already has an entry for the given initial state.
-            let memo = memos.get(pos);
-            if (!memo) {
+            let mmo = parseMemos.get(pos);
+            if (!mmo) {
                 // The memo table does *not* have an entry, so this is the first attempt to apply this rule with this
                 // initial state. The first thing we do is create a memo table entry, which is marked as *unresolved*.
                 // All future applications of this rule with the same initial state will find this memo. If a future
                 // application finds the memo still unresolved, then we know we have encountered left-recursion.
-                memo = {resolved: false, isLeftRecursive: false, result: {node: FAIL, posᐟ: 0}};
-                memos.set(pos, memo);
+                mmo = {resolved: false, isLeftRecursive: false, result: {node: PARSE_FAIL, posᐟ: 0}};
+                parseMemos.set(pos, mmo);
 
                 // Now that the unresolved memo is in place, apply the rule, and resolve the memo with the result. At
                 // this point, any left-recursive paths encountered during application are guaranteed to have been noted
                 // and aborted (see below).
-                if (!expr.parse(text, pos, memo.result)) memo.result.node = FAIL;
-                memo.resolved = true;
+                if (!expr.parse(text, pos, mmo.result)) mmo.result.node = PARSE_FAIL;
+                mmo.resolved = true;
 
                 // If we did *not* encounter left-recursion, then we have simple memoisation, and the result is final.
-                if (!memo.isLeftRecursive) {
-                    Object.assign(result, memo.result);
-                    return result.node !== FAIL;
+                if (!mmo.isLeftRecursive) {
+                    Object.assign(result, mmo.result);
+                    return result.node !== PARSE_FAIL;
                 }
 
                 // If we get here, then the above application of the rule invoked itself left-recursively, but we
@@ -53,54 +50,54 @@ function memo(expr: Production): Production {
                 // and consumes more input than the previous iteration did, in which case we update the memo with the
                 // new result. We thus 'grow' the result, stopping when application either fails or does not consume
                 // more input, at which point we take the result of the previous iteration as final.
-                while (memo.result.node !== FAIL) {
-                    if (!expr.parse(text, pos, result)) result.node = FAIL;
-                    if (result.node === FAIL || result.posᐟ <= memo.result.posᐟ) break;
-                    Object.assign(memo.result, result);
+                while (mmo.result.node !== PARSE_FAIL) {
+                    if (!expr.parse(text, pos, result)) result.node = PARSE_FAIL;
+                    if (result.node === PARSE_FAIL || result.posᐟ <= mmo.result.posᐟ) break;
+                    Object.assign(mmo.result, result);
                 }
             }
-            else if (!memo.resolved) {
+            else if (!mmo.resolved) {
                 // If we get here, then we have already applied the rule with this initial state, but not yet resolved
                 // it. That means we must have entered a left-recursive path of the rule. All we do here is note that
                 // the rule application encountered left-recursion, and return with failure. This means that the initial
                 // application of the rule for this initial state can only possibly succeed along a non-left-recursive
                 // path. More importantly, it means the parser will never loop endlessly on left-recursive rules.
-                memo.isLeftRecursive = true;
+                mmo.isLeftRecursive = true;
                 return false;
             }
 
             // We have a resolved memo, so the result of the rule application for the given initial state has already
             // been computed. Return it from the memo.
-            Object.assign(result, memo.result);
-            return result.node !== FAIL;
+            Object.assign(result, mmo.result);
+            return result.node !== PARSE_FAIL;
         },
 
         unparse(node, pos, result) {
             // Check whether the memo table already has an entry for the given initial state.
-            let memos2 = memos.get(node);
+            let memos2 = unparseMemos.get(node);
             if (memos2 === undefined) {
                 memos2 = new Map();
-                memos.set(node, memos2);
+                unparseMemos.set(node, memos2);
             }
-            let memo = memos2.get(pos);
-            if (!memo) {
+            let mmo = memos2.get(pos);
+            if (!mmo) {
                 // The memo table does *not* have an entry, so this is the first attempt to apply this rule with this
                 // initial state. The first thing we do is create a memo table entry, which is marked as *unresolved*.
                 // All future applications of this rule with the same initial state will find this memo. If a future
                 // application finds the memo still unresolved, then we know we have encountered left-recursion.
-                memo = {resolved: false, isLeftRecursive: false, result: {src: FAIL, posᐟ: 0}};
-                memos2.set(pos, memo);
+                mmo = {resolved: false, isLeftRecursive: false, result: {text: UNPARSE_FAIL, posᐟ: 0}};
+                memos2.set(pos, mmo);
 
                 // Now that the unresolved memo is in place, apply the rule, and resolve the memo with the result. At
                 // this point, any left-recursive paths encountered during application are guaranteed to have been noted
                 // and aborted (see below).
-                if (!expr.unparse(node, pos, memo.result)) memo.result.src = FAIL;
-                memo.resolved = true;
+                if (!expr.unparse(node, pos, mmo.result)) mmo.result.text = UNPARSE_FAIL;
+                mmo.resolved = true;
 
                 // If we did *not* encounter left-recursion, then we have simple memoisation, and the result is final.
-                if (!memo.isLeftRecursive) {
-                    Object.assign(result, memo.result);
-                    return result.text !== FAIL;
+                if (!mmo.isLeftRecursive) {
+                    Object.assign(result, mmo.result);
+                    return result.text !== UNPARSE_FAIL;
                 }
 
                 // If we get here, then the above application of the rule invoked itself left-recursively, but we
@@ -110,39 +107,39 @@ function memo(expr: Production): Production {
                 // and consumes more input than the previous iteration did, in which case we update the memo with the
                 // new result. We thus 'grow' the result, stopping when application either fails or does not consume
                 // more input, at which point we take the result of the previous iteration as final.
-                while (memo.result.src !== FAIL) {
-                    if (!expr.unparse(node, pos, result)) result.text = FAIL;
+                while (mmo.result.text !== UNPARSE_FAIL) {
+                    if (!expr.unparse(node, pos, result)) result.text = UNPARSE_FAIL;
 
                     // TODO: break cases:
                     // anything --> same thing (covers all string cases, since they can only be same or shorter)
                     // some node --> some different non-empty node (assert: should never happen!)
-                    if (result.text === FAIL) break;
-                    if (result.posᐟ === memo.result.posᐟ) break;
+                    if (result.text === UNPARSE_FAIL) break;
+                    if (result.posᐟ === mmo.result.posᐟ) break;
                     if (!isFullyConsumed(node, result.posᐟ)) break;
-                    Object.assign(memo.result, result);
+                    Object.assign(mmo.result, result);
                 }
             }
-            else if (!memo.resolved) {
+            else if (!mmo.resolved) {
                 // If we get here, then we have already applied the rule with this initial state, but not yet resolved
                 // it. That means we must have entered a left-recursive path of the rule. All we do here is note that
                 // the rule application encountered left-recursion, and return with failure. This means that the initial
                 // application of the rule for this initial state can only possibly succeed along a non-left-recursive
                 // path. More importantly, it means the parser will never loop endlessly on left-recursive rules.
-                memo.isLeftRecursive = true;
+                mmo.isLeftRecursive = true;
                 return false;
             }
 
             // We have a resolved memo, so the result of the rule application for the given initial state has already
             // been computed. Return it from the memo.
-            Object.assign(result, memo.result);
-            return result.text !== FAIL;
+            Object.assign(result, mmo.result);
+            return result.text !== UNPARSE_FAIL;
         },
     };
-};
+}
 
 
-// TODO: for parse...
-const FAIL = Symbol('FAIL');
+const PARSE_FAIL = Symbol('FAIL');
 
-// TODO: for unparse...
-const FAIL = '\uD800'; // NB: this is an invalid code point (lead surrogate with no pair). It is used as a sentinel.
+
+// NB: this is an invalid code point (lead surrogate with no pair). It is used as a sentinel.
+const UNPARSE_FAIL = '\uD800';
