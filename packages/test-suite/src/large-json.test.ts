17d44f0ab0d04535826dd1bda9f50dcf03b381c5
Troy Gerwien <yortus@gmail.com>
2020-05-13 18:00:46 +0800

    add unparse test for large json doc

--- packages/test-suite/src/large-json.test.ts
+++ packages/test-suite/src/large-json.test.ts
@@ -12,12 +12,14 @@ const jsonDocPath = path.join(__dirname, '../fixtures/documents/1mb.json');
 
 
 describe(`Procesing a large JSON document`, async () => {
-
-    it('parses', async () => {
+    before(() => {
         compile({
             main: inputPath,
             outFile: outputPath,
         });
+    });
+
+    it('parses', async () => {
         let {parse} = require(outputPath);
 
         let text = await fs.readFile(jsonDocPath, 'utf8');
@@ -29,5 +31,19 @@ describe(`Procesing a large JSON document`, async () => {
         expect(json2).to.deep.equal(json1);
         console.log(`JSON.parse took ${t1 - t0}ms`);
         console.log(`penc parse took ${t2 - t1}ms`);
+    });
+
+    it('prints', () => {
+        let {unparse} = require(outputPath);
+
+        let json = require(jsonDocPath);
+        let t0 = new Date().getTime();
+        let text1 = JSON.stringify(json);
+        let t1 = new Date().getTime();
+        let text2 = unparse(json);
+        let t2 = new Date().getTime();
+        expect(text2).to.equal(text1);
+        console.log(`JSON.stringify took ${t1 - t0}ms`);
+        console.log(`penc unparse took ${t2 - t1}ms`);
     });
 });
