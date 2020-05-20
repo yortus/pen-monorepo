8c7bd5120e96826d7b0f5ef7859e1f967f07852a
Troy Gerwien <yortus@gmail.com>
2020-05-20 09:41:49 +0800

    add support for heap profiling

--- packages/test-suite/bench/scripts/parse-1mb.js
+++ packages/test-suite/bench/scripts/parse-1mb.js
@@ -1,7 +1,11 @@
+// Use this script for performance analysis, eg perf/mem profiling, debugging, etc
+
+// Uncomment the following line to generate a .heapsnapshot file
+//require('heapdump').writeSnapshot();
+
+
 let {parse} = require('../../baselines/penc-output/json.js');
 let json = require('../../fixtures/documents/1mb.json');
-
-
 let text = JSON.stringify(json, undefined, 4);
 let t0 = new Date().getTime();
 json = parse(text);
