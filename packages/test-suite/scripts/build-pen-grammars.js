7a8f9dd5b4ef3fab5a7f2ddb624e7b5592a00c55
Troy Gerwien <yortus@gmail.com>
2020-05-21 19:48:51 +0800

    tweak postbulid script

--- packages/test-suite/scripts/build-pen-grammars.js
+++ packages/test-suite/scripts/build-pen-grammars.js
@@ -12,19 +12,21 @@ for (let i = 0; i < inputFiles.length; ++i) {
     inputName = path.basename(inputFiles[i]);
     inputFile = path.resolve(SRC_PATH, inputFiles[i]);
     outputFile = path.resolve(DIST_PATH, inputFiles[i]).replace(/\.pen$/g, '') + '.js';
-    // TODO: fix... ALSO needs to rebuild if penc is newer
-    // let inputTime = fs.statSync(inputFile).mtime.getTime();
-    // let outputTime = fs.existsSync(outputFile) ? fs.statSync(outputFile).mtime.getTime() : 0;
+    compilerFile = require.resolve('penc');
+    let inputTime = fs.statSync(inputFile).mtime.getTime();
+    let outputTime = fs.existsSync(outputFile) ? fs.statSync(outputFile).mtime.getTime() : 0;
+    let compilerTime = fs.statSync(compilerFile).mtime.getTime();
 
+    // Re-build if either the input grammar or the compiler changed since the output file was written.
     process.stdout.write(`Building ${inputName}... `);
-    // if (outputTime > inputTime) {
-    //     process.stdout.write('SKIPPED (already up-to-date)\n');
-    // }
-    // else {
+    if (outputTime > inputTime && outputTime > compilerTime) {
+        process.stdout.write('SKIPPED (already up-to-date)\n');
+    }
+    else {
         compile({
             main: inputFile,
             outFile: outputFile,
         });
         process.stdout.write('DONE\n');
-    // }
+    }
 }
