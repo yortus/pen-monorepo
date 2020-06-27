da9aa9f6f4b8c9527b8005cd8c01a9f3d8a69b20
Troy Gerwien <yortus@gmail.com>
2020-06-27 17:51:34 +0800

    WIP3 on computeNodeHashes

--- packages/core/penc/src/transforms/06-compute-node-hashes/compute-node-hashes.ts
+++ packages/core/penc/src/transforms/06-compute-node-hashes/compute-node-hashes.ts
@@ -28,35 +28,38 @@ export function computeNodeHashes(program: Program<Metadata>) {
             case 'ApplicationExpression': return {kind: 'APP', l: sigs.get(n.lambda), a: sigs.get(n.argument)};
             // case 'Binding': return;
             case 'BindingLookupExpression': return {kind: 'BLE', m: sigs.get(n.module), n: n.bindingName};
-            case 'BooleanLiteralExpression':
-            case 'NullLiteralExpression':
-            case 'NumericLiteralExpression': return {kind: 'LIT', value: n.value};
-            // case 'ExtensionFile': return;
-            // case 'FieldExpression': return;
-            // case 'ImportExpression': return;
+            case 'BooleanLiteralExpression': return {kind: 'LIT', value: n.value};
+            case 'ExtensionFile': return {kind: 'EXF', p: n.path};
+            case 'FieldExpression': return {kind: 'FEX', n: sigs.get(n.name), v: sigs.get(n.value)};
+            case 'ImportExpression': return sigs.get(program.sourceFiles.get(n.sourceFilePath)!);
             // // case 'LambdaExpression': TODO: ...
             case 'ListExpression': return {kind: 'LST', e: n.elements.map(e => sigs.get(e))};
             case 'Module':
-                // return {kind: 'MDL', b: n.bindings};
+                // return {kind: 'MOD', b: n.bindings};
                 // TODO!!!
                 // - binding order not important
                 // - need to 'normalise' - ie:
                 //   a) get a flat list of binding names from either VariablePattern or ModulePatternName nodes
                 //   b) link each binding name to appropriate rhs signature - either direct, or equiv of BLE
                 return;
-            // case 'ModuleExpression': return;
+            case 'ModuleExpression': return sigs.get(n.module);
             // case 'ModulePattern': return;
             // case 'ModulePatternName': return;
             case 'NotExpression': return {kind: 'NOT', e: sigs.get(n.expression)};
+            case 'NullLiteralExpression': return {kind: 'LIT', value: n.value};
+            case 'NumericLiteralExpression': return {kind: 'LIT', value: n.value};
             case 'ParenthesisedExpression': return sigs.get(n);
-            // case 'PenSourceFile': return;
-            // case 'Program': return;
+            case 'PenSourceFile': return sigs.get(n.module);
+            case 'Program': return {kind: 'PRG'}; // only one of these
             case 'QuantifiedExpression': return {kind: 'QUA', e: sigs.get(n.expression), q: n.quantifier};
             case 'RecordExpression': return {kind: 'REC', f: n.fields.map(f => sigs.get(f))};
-            // case 'ReferenceExpression': return;
+            case 'ReferenceExpression':
+                // TODO: must be same hash value as its referent - how to look it up?
+                program.meta.symbolTable.getSymbolById(n.meta.symbolId);
+                return;
             case 'SelectionExpression': return {kind: 'SEL', e: n.expressions.map(e => sigs.get(e))};
             case 'SequenceExpression': return {kind: 'SEQ', e: n.expressions.map(e => sigs.get(e))};
-            case 'StaticField': return {kind: 'FLD', n: n.name, v: sigs.get(n.value)};
+            case 'StaticField': return {kind: 'STF', n: n.name, v: sigs.get(n.value)};
             case 'StringLiteralExpression': return {kind: 'STR', value: n.value, a: n.abstract, c: n.concrete};
             // case 'VariablePattern': return;
             // default: ((assertNoKindsLeft: never) => { throw new Error(`Unhandled node ${assertNoKindsLeft}`); })(n);
