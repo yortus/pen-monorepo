14f0417e61be4a274da4daf0b26a168d40c949a9
Troy Gerwien <yortus@gmail.com>
2020-08-18 20:43:09 +0800

    add TODO comment

--- packages/core/penc/src/ast/index.ts
+++ packages/core/penc/src/ast/index.ts
@@ -1,1 +1,18 @@
 export * from './nodes';
+
+
+// TODO:
+// rename this dir
+// - really want to have a type for each transform input/output
+// - 'ast' is too specific - not every stage inputs/outputs an ast - just the early stages
+// - intermediate forms
+// - other synonyms? form, stage, phase, state, **representation**
+// - A: go with 'REPRESENTATION'
+
+
+// desired exports: a 'type' for each representation:
+// - SourceAst
+// - DesugaredAst
+// - ResolvedAst
+// - Flat???Something
+
