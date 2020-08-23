f6619382ff92139687f8593665fbfbd6aba537a0
Troy Gerwien <yortus@gmail.com>
2020-08-23 20:52:24 +0800

    new ast helper types WIP

--- packages/core/penc/src/ast/nodes/program.ts
+++ packages/core/penc/src/ast/nodes/program.ts
@@ -1,4 +1,4 @@
-import {NodeKind} from '../../ast-nodes';
+import {Node, NodeKind} from '../../ast-nodes';
 import {AbsPath} from '../../utils';
 import {Module} from './module';
 
@@ -9,3 +9,23 @@ export interface Program<KS extends NodeKind = NodeKind> {
     readonly mainPath: AbsPath;
     readonly startSymbolId?: string;
 }
+
+
+
+
+// TODO: temp testing........
+type NodeKindsFromProgram<P extends Program<any>> = P extends Program<infer KS> ? KS : never;
+type NodeFromNodeKind<KS extends NodeKind, K extends NodeKind, N = Node<KS>> = N extends {kind: K} ? N : never;
+type NodeFromProgram<P extends Program<any>, K extends NodeKind> = NodeFromNodeKind<NodeKindsFromProgram<P>, K>;
+
+
+
+type MyProgram = Program<'Program' | 'Module' | 'LocalBinding' | 'StringLiteralExpression'>;
+type Kinds1 = NodeKindsFromProgram<MyProgram>; //           ✓
+type Module2 = NodeFromProgram<MyProgram, 'Module'>; //     ✓
+
+
+// So representations can each be a RepNameProgram<KS> type
+// - export from where?
+// - one per file, or all in one representations.ts file?
+// - program.ts or representation.ts
