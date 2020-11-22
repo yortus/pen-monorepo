08bd2002b2ccd3391b49016c63c0c04a3d6bff04
Troy Gerwien <yortus@gmail.com>
2020-11-22 09:26:16 +0800

    remove old/dead code

--- doc/samples/compile-test.definition-map.js
+++ doc/samples/compile-test.definition-map.js
@@ -3,68 +3,18 @@ const definitions =
   {
     kind: "Definition",
     definitionId: 0,
-    moduleId: "@@root",
-    localName: "@compile_test",
+    moduleId: "@compile_test",
+    localName: "std",
     value: {
-      kind: "Module",
-      moduleId: "@compile_test",
+      kind: "Reference",
+      definitionId: 18,
     },
   },
   {
     kind: "Definition",
     definitionId: 1,
-    moduleId: "@@root",
-    localName: "@compile_test_modexpr",
-    value: {
-      kind: "Module",
-      moduleId: "@compile_test_modexpr",
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 2,
-    moduleId: "@@root",
-    localName: "@compile_test_modexpr2",
-    value: {
-      kind: "Module",
-      moduleId: "@compile_test_modexpr2",
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 3,
-    moduleId: "@@root",
-    localName: "@std",
-    value: {
-      kind: "Module",
-      moduleId: "@std",
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 4,
-    moduleId: "@compile_test",
-    localName: "std",
-    value: {
-      kind: "Reference",
-      definitionId: 3,
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 5,
     moduleId: "@compile_test",
     localName: "digits",
-    value: {
-      kind: "Reference",
-      definitionId: 1,
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 6,
-    moduleId: "@compile_test",
-    localName: "des",
     value: {
       kind: "Reference",
       definitionId: 11,
@@ -72,29 +22,19 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 7,
+    definitionId: 2,
+    moduleId: "@compile_test",
+    localName: "des",
+    value: {
+      kind: "Reference",
+      definitionId: 8,
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 3,
     moduleId: "@compile_test",
     localName: "ref",
-    value: {
-      kind: "Reference",
-      definitionId: 6,
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 8,
-    moduleId: "@compile_test",
-    localName: "mem",
-    value: {
-      kind: "Reference",
-      definitionId: 12,
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 9,
-    moduleId: "@compile_test",
-    localName: "xxx",
     value: {
       kind: "Reference",
       definitionId: 2,
@@ -102,17 +42,134 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 10,
+    definitionId: 4,
     moduleId: "@compile_test",
-    localName: "yyy",
+    localName: "mem",
     value: {
       kind: "Reference",
-      definitionId: 12,
+      definitionId: 9,
     },
   },
   {
     kind: "Definition",
-    definitionId: 11,
+    definitionId: 5,
+    moduleId: "@compile_test",
+    localName: "xxx",
+    value: {
+      kind: "Reference",
+      definitionId: 13,
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 6,
+    moduleId: "@compile_test",
+    localName: "yyy",
+    value: {
+      kind: "Reference",
+      definitionId: 9,
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 7,
+    moduleId: "@@root",
+    localName: "@compile_test",
+    value: {
+      kind: "Module",
+      moduleId: "@compile_test",
+      parentModuleId: undefined,
+      bindings: [
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "std",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 0,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "digits",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 1,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "one",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 2,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "ref",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 3,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "mem",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 4,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "xxx",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 5,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "yyy",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 6,
+          },
+          exported: false,
+        },
+      ],
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 8,
     moduleId: "@compile_test_modexpr",
     localName: "one",
     value: {
@@ -125,7 +182,7 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 12,
+    definitionId: 9,
     moduleId: "@compile_test_modexpr",
     localName: "two",
     value: {
@@ -135,27 +192,101 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 13,
+    definitionId: 10,
     moduleId: "@compile_test_modexpr",
     localName: "outer",
     value: {
       kind: "Reference",
-      definitionId: 8,
+      definitionId: 4,
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 11,
+    moduleId: "@@root",
+    localName: "@compile_test_modexpr",
+    value: {
+      kind: "Module",
+      moduleId: "@compile_test_modexpr",
+      parentModuleId: "@compile_test",
+      bindings: [
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "one",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 8,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "two",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 9,
+          },
+          exported: false,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "outer",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 10,
+          },
+          exported: false,
+        },
+      ],
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 12,
+    moduleId: "@compile_test_modexpr2",
+    localName: "d",
+    value: {
+      kind: "Reference",
+      definitionId: 1,
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 13,
+    moduleId: "@@root",
+    localName: "@compile_test_modexpr2",
+    value: {
+      kind: "Module",
+      moduleId: "@compile_test_modexpr2",
+      parentModuleId: "@compile_test",
+      bindings: [
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "d",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 12,
+          },
+          exported: false,
+        },
+      ],
     },
   },
   {
     kind: "Definition",
     definitionId: 14,
-    moduleId: "@compile_test_modexpr2",
-    localName: "d",
-    value: {
-      kind: "Reference",
-      definitionId: 5,
-    },
-  },
-  {
-    kind: "Definition",
-    definitionId: 15,
     moduleId: "@std",
     localName: "char",
     value: {
@@ -166,7 +297,7 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 16,
+    definitionId: 15,
     moduleId: "@std",
     localName: "f64",
     value: {
@@ -177,7 +308,7 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 17,
+    definitionId: 16,
     moduleId: "@std",
     localName: "i32",
     value: {
@@ -188,13 +319,74 @@ const definitions =
   },
   {
     kind: "Definition",
-    definitionId: 18,
+    definitionId: 17,
     moduleId: "@std",
     localName: "memoise",
     value: {
       kind: "Intrinsic",
       name: "memoise",
       path: "V:/projects/oss/pen-monorepo/packages/core/penc/dist/deps/std.pen.js",
+    },
+  },
+  {
+    kind: "Definition",
+    definitionId: 18,
+    moduleId: "@@root",
+    localName: "@std",
+    value: {
+      kind: "Module",
+      moduleId: "@std",
+      parentModuleId: undefined,
+      bindings: [
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "char",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 14,
+          },
+          exported: true,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "f64",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 15,
+          },
+          exported: true,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "i32",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 16,
+          },
+          exported: true,
+        },
+        {
+          kind: "Binding",
+          left: {
+            kind: "Identifier",
+            name: "memoise",
+          },
+          right: {
+            kind: "Reference",
+            definitionId: 17,
+          },
+          exported: true,
+        },
+      ],
     },
   },
 ];
--- packages/core/penc/src/transforms/03-create-definition-map/create-definition-map.ts
+++ packages/core/penc/src/transforms/03-create-definition-map/create-definition-map.ts
@@ -1,4 +1,4 @@
-import {Definition, Expression, mapNode, MemberExpression, Module, Reference, traverseNode} from '../../abstract-syntax-trees';
+import {Binding, Definition, Expression, mapNode, MemberExpression, Module, Reference, traverseNode} from '../../abstract-syntax-trees';
 import {DefinitionMap, definitionMapKinds, ModuleMap} from '../../representations';
 import {assert} from '../../utils';
 
@@ -11,21 +11,10 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
     let definitions = [] as Definition[];
     let references = [] as {name: string, moduleId: string, ref: Reference}[];
 
-    // Define all modules in the root scope.
+    // Define a root scope.
     const ROOT_MODULE_ID = '@@root'; // TODO: ensure can never clash with any identifier name or moduleId
     const rootScope: Scope = Object.create(null);
     scopesByModuleId.set(ROOT_MODULE_ID, rootScope);
-    for (const module of Object.values(modulesById)) {
-        const definition: Definition = {
-            kind: 'Definition',
-            definitionId: definitions.length,
-            moduleId: ROOT_MODULE_ID,
-            localName: module.moduleId,
-            value: {kind: 'Module', moduleId: module.moduleId} as Module, // TODO: yukky lie in type system - but full ast not needed here - how to fix?
-        };
-        definitions.push(definition);
-        rootScope[module.moduleId] = definition;
-    }
 
     // Helper function to add a definition for `name` into the given module's scope.
     function define(name: string, moduleId: string, value: Expression | Module): Definition {
@@ -48,7 +37,7 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
         return definition;
     }
 
-    // TODO: ...
+    // TODO: doc...
     function lookup(name: string, moduleId: string): Definition {
         const scope = scopesByModuleId.get(moduleId);
         assert(scope); // sanity check
@@ -69,10 +58,12 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
         scopesByModuleId.set(moduleId, scope);
 
         // Create a definition for each local name in the module.
-        for (let {left, right} of bindings) {
+        let newBindings: Binding[] = [];
+        for (let {left, right, exported} of bindings) {
             // For a simple `name = value` binding, create a single definition.
             if (left.kind === 'Identifier') {
-                define(left.name, moduleId, right);
+                const {definitionId} = define(left.name, moduleId, right);
+                newBindings.push({kind: 'Binding', left, right: {kind: 'Reference', definitionId}, exported});
             }
 
             // For a destructured `{a, b} = module` binding, create a definition for each name in the lhs.
@@ -83,40 +74,22 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
                         module: right,
                         member: {kind: 'Identifier', name},
                     };
-                    define(alias ?? name, moduleId, expr);
+                    const {definitionId} = define(alias ?? name, moduleId, expr);
+                    newBindings.push({
+                        kind: 'Binding',
+                        left: {kind: 'Identifier', name},
+                        right: {kind: 'Reference', definitionId},
+                        exported,
+                    });
                 }
             }
         }
+
+        // Create a definition for the module itself.
+        // TODO: better to make this appear *before* its bindings defns in the defns array?
+        // - or just use a map (eg with string keys) instead of ints for defnIds? Why use ints? No reason really except easily unique
+        define(moduleId, ROOT_MODULE_ID, {kind: 'Module', moduleId, parentModuleId, bindings: newBindings});
     }
-
-
-
-
-    // TODO: ...
-    // Traverse the expression for each definition:
-    // - resolve Identifier to Reference
-    //   - just do a 'lookup'
-    //   - but need to loop, since lookup result itself may refer to an Identifier or MemberExpression
-    //   - also need to prevent infinite loop by remembering what defns have already been visited and never revisiting
-    //     - eg a = b   b = c   c = a
-    // - resolve MemberExpression to Reference
-    //   - form: `module`.`member`
-    //   - Q: what expr kinds can `module` potentially be before resolution?
-    //     - Identifier like '@std' referring directly to a Module
-    //     - Identifier like 'mod1' referring to a definition
-    //     - MemberExpression, like `a.b` in `a.b.c`
-    //     - ApplicationExpression, like `foo()` in `foo().bar`
-    //       - just disallow this for now (produce an error) and come back to it later
-    //     - *nothing* else would ever be valid - not even ModuleExpression since they are no longer in the AST
-    //   - call self to 'resolve' the `module` expression
-    //   - once resolved, assert that the `module` expression is a module
-    //     - how/what exactly?
-    //   - just do a lookup of the `member` name in the `module` expression
-    //   - but need to loop, since lookup result itself may refer to an Identifier or MemberExpression
-    //   - also need to prevent infinite loop by remembering what defns have already been visited and never revisiting
-    //     - eg a = {b = a.b}
-    // - (leave parens alone in this transform)
-
 
     // Resolve all Identifier nodes (except MemberExpression#member - that is resolved next)
     for (let def of definitions) {
@@ -163,148 +136,9 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
         Object.assign(def, {value: newValue}); // TODO: messy overwrite of readonly prop - better/cleaner way?
     }
 
-
-
-    // // The dereference function, closed over the given AST.
-    // function deref(expr: Expression): Expression {
-    //     const seen = [expr];
-    //     while (true) {
-
-    //         let tgt: Expression | undefined;
-    //         if (expr.kind === 'Identifier') {
-    //             if (expr.name.startsWith('@')) { // TODO: synthetic id for module - won't be in symbol table
-    //                 const module = modulesById[expr.name];
-    //                 assert(module);
-    //                 console.log(`REF to module ${module.moduleId}`);
-    //             }
-    //             else {
-
-    //             }
-
-
-    //         }
-
-
-
-    //         // If `expr` is a par|ref|mem expression, try to resolve to its target expression.
-    //         if (expr.kind === 'ParenthesisedExpression') {
-    //             tgt = expr.expression;
-    //         }
-    //         else if (expr.kind === 'GlobalReferenceExpression') {
-    //             // Global references can always be resolved to their target node (which may be another deref'able node).
-    //             tgt = resolveReference(expr);
-    //         }
-    //         else if (expr.kind === 'MemberExpression') {
-    //             // Member expressions _may_ have an identifiable target node, but not always.
-    //             tgt = resolveMember(expr);
-    //         }
-
-    //         // If the target expression for `expr` could not be determined, return `expr` unchanged.
-    //         if (tgt === undefined) return expr;
-
-    //         // If `expr` resolved to a target expression that isn't a par|ref|mem expression, return the target expression.
-    //         if (tgt.kind !== 'GlobalReferenceExpression' && tgt.kind !== 'MemberExpression' && tgt.kind !== 'ParenthesisedExpression') return tgt;
-
-    //         // If the target expression is still a par|ref|mem expression, keep iterating, but prevent an infinite loop.
-    //         if (seen.includes(tgt)) {
-    //             // TODO: improve diagnostic message, eg line/col ref
-    //             const name = tgt.kind === 'GlobalReferenceExpression' ? tgt.globalName : tgt.kind === 'MemberExpression' ? tgt.member.name : '(?)'; // TODO: fix par case!
-    //             throw new Error(`'${name}' is circularly defined`);
-    //         }
-    //         seen.push(tgt);
-    //         expr = tgt;
-    //     }
-    // }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // TODO: for each module...
-    for (let {moduleId, parentModuleId, bindings} of Object.values(modulesById)) {
-        console.log(`MODULE ${moduleId}`);
-        let parentScope = parentModuleId ? scopesByModuleId.get(parentModuleId) : rootScope;
-        assert(parentScope); // TODO: sanity check - relies on specific order of modules in module map - fix this
-
-        // Create a new scope for this module.
-        let scope = Object.create(parentScope);
-        scopesByModuleId.set(moduleId, scope);
-
-        // TODO: for each binding...
-        for (let {left, right} of bindings) {
-            // TODO: ...
-            [] = [left, right];
-
-
-            // TODO: doc... what are we doing with `right` here?
-            // - `right` is an Expression
-            // - replace every Identifier with a Reference
-
-            // right = mapNode(right, rec => ({
-
-            //     // TODO: what is this for?
-            //     MemberExpression: mem => {
-            //         // collect 1 reference (in specific scope)
-            //         // TODO: not actually collecting the reference yet...
-            //         console.log(`    REF S?.${mem.member.name}`);
-
-
-            //         // TODO: get the moduleId referred to by mem.module (must be statically resolvable)
-            //         // - recursive search, based on `createDereferencer`
-
-
-            //         let memᐟ = {...mem, module: rec(mem.module)};
-            //         return memᐟ;
-            //     },
-    
-            //     // Replace every Identifier with an equivalent Reference.
-            //     Identifier: nam => {
-            //         // collect 1 reference (in enclosing scope)
-            //         console.log(`    REF ${nam.name}`);
-    
-            //         // Create placeholder Reference that will be backpatched later when defId is known
-            //         let ref: Reference = {kind: 'Reference', definitionId: undefined!};
-            //         refExprs.push({name: nam.name, moduleId, ref});
-            //         return ref;
-            //     },
-            // }));
-        }
-        console.log(`END MODULE ${moduleId}`);
-    }
+    // TODO: allowed node kinds says there should be no Module or Identifier node kinds in the definitionMap.
+    // - we still have Module nodes, and they can be referenced from expressions (eg std, xxx in compile-test)
+    // - we still have Identifier nodes, but only in the Binding#left within Module nodes
 
 
 
@@ -328,67 +162,8 @@ export function createDefinitionMap({modulesById}: ModuleMap): DefinitionMap {
 
     if (1 !== 1 + 1) return null!;
 
-
-
-    //let symbolTable: unknown;
-    //let counter = 0;
-    // let definitions = [] as Definition[];
-    // mapMap(program.modulesByAbsPath, mod => {
-    //     return mapNode2(mod, rec => ({
-
-    //         // // Replace each LocalMultiBinding with a series of LocalBindings
-    //         // Module: mod => {
-    //         //     let bindings = [] as LocalBinding[];
-    //         //     for (let binding of mod.bindings) {
-    //         //         assert(sourceNodeKinds.includes(binding));
-    //         //         if (binding.kind === 'LocalBinding') {
-    //         //             bindings.push(rec(binding));
-    //         //         }
-    //         //         else {
-    //         //             // Introduce a new local binding for the RHS.
-    //         //             // TODO: ensure no collisions with program names. '$1' etc is ok since '$' isn't allowed in PEN ids.
-    //         //             let localName = `$${++counter}`;
-    //         //             let {names, value, exported} = binding;
-    //         //             bindings.push({
-    //         //                 kind: 'LocalBinding',
-    //         //                 localName,
-    //         //                 value: rec(value),
-    //         //                 exported
-    //         //             });
-
-    //         //             // Introduce a local binding for each name in the LHS
-    //         //             for (let {name: member, alias} of names) {
-    //         //                 let ref: LocalReferenceExpression;
-    //         //                 let mem: MemberExpression;
-    //         //                 ref = {kind: 'LocalReferenceExpression', localName};
-    //         //                 mem = {kind: 'MemberExpression', module: ref, member};
-    //         //                 bindings.push({
-    //         //                     kind: 'LocalBinding',
-    //         //                     localName: alias ?? member,
-    //         //                     value: mem,
-    //         //                     exported
-    //         //                 });
-    //         //             }
-    //         //         }
-    //         //     }
-
-    //         //     let modᐟ = {...mod, bindings};
-    //         //     return modᐟ;
-    //         // },
-    //     }));
-    // });
-
     return null!; /*{
         definitions,
         startSomething: '????',
     };*/
-
-
-
-
-
-    // // TODO: temp testing...
-    // function resolveExpressionToModuleId(expr: Expression) {
-
-    // }
 }
