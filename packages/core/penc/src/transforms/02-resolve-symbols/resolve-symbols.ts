8a14432a6a0aa5b439618a25583a9d1f8815e294
Troy Gerwien <yortus@gmail.com>
2021-02-01 17:00:31 +0800

    fix genexpr handling WIP4

--- packages/core/penc/src/transforms/02-resolve-symbols/resolve-symbols.ts
+++ packages/core/penc/src/transforms/02-resolve-symbols/resolve-symbols.ts
@@ -22,20 +22,105 @@ export function resolveSymbols(ast: V.AST<200>): V.AST<300> {
         },
     });
 
-    // TODO: temp testing...
-    let resolved = internalResolve({
-        gen: {
-            kind: 'GenericExpression',
-            param: 'DUMMY', // TODO: this param is never referenced, so name doesn't matter. Remove it somehow?
-            body: ast.start,
+    // STEP 1: Traverse the AST, creating a scope for each module/letexpr/genexpr, and a symbol for each binding name/value pair.
+    let env = rootScope;
+    const identifiers = new Map<V.Identifier, Scope>();
+    const memberExprs = [] as V.MemberExpression<300>[];
+    const startᐟ = mapNode(ast.start, rec => ({
+        GenericExpression: ({param, body}): V.GenericExpression<300> => {
+
+            // Create a closure and nested scope for this generic expression.
+            const surroundingClosureId = currentClosureId;
+            currentClosureId = 'C' + String(++closureCounter).padStart(6, '0');
+            env = env.createNestedScope();
+
+            // Create a symbol for the generic parameter, whose value is specially marked as a 'placeholder'.
+            let placeholder: V.Identifier = {kind: 'Identifier', name: '', placeholder: true};
+            const {uniqueName} = env.insert(param, placeholder);
+            Object.assign(placeholder, {name: uniqueName}); // TODO: cleaner way than in-place update?
+
+            // Traverse the body expression in the new scope, then revert to the surrounding scope before returning.
+            const bodyᐟ = rec(body);
+            env = env.surroundingScope;
+            currentClosureId = surroundingClosureId;
+            return {kind: 'GenericExpression', param, body: bodyᐟ};
         },
-        arg: {kind: 'Module', bindings: {}},
-        env: rootScope,
-    });
+        Identifier: id => {
+            if (id.unique) return id;
+            // TODO: explain tracking...
+            // Collect every Identifier encountered during the traversal, to be resolved in step 2.
+            const idᐟ = {...id};
+            identifiers.set(idᐟ, env);
+            return idᐟ;
+        },
+        LetExpression: le => {
+            // Create a nested scope for this let expression.
+            env = env.createNestedScope();
+
+            // Create a symbol for each local name in the let expression.
+            const bindings = {} as Record<string, V.Identifier>;
+            for (const [name, expr] of Object.entries(le.bindings)) {
+                const {uniqueName} = env.insert(name, rec(expr));
+                bindings[name] = {kind: 'Identifier', name: uniqueName, unique: true};
+            }
+
+            // Recursively resolve the main expression in the nested scope.
+            const expression = rec(le.expression);
+
+            // Pop back out to the surrounding scope before returning.
+            env = env.surroundingScope;
+            return expression;
+        },
+        MemberExpression: mem => {
+            // TODO: explain tracking...
+            // Collect every MemberExpression encountered during the traversal, to be resolved in step 3.
+            const memᐟ = {...mem, module: rec(mem.module)};
+            memberExprs.push(memᐟ);
+            return memᐟ;
+        },
+        Module: module => {
+            // Create a nested scope for this module.
+            env = env.createNestedScope();
+
+            // Create a symbol for each local name in the module.
+            const bindings = {} as Record<string, V.Identifier>;
+            for (const [name, expr] of Object.entries(module.bindings)) {
+                const {uniqueName} = env.insert(name, rec(expr));
+                bindings[name] = {kind: 'Identifier', name: uniqueName, unique: true};
+            }
+
+            // Pop back out to the surrounding scope before returning.
+            env = env.surroundingScope;
+            return {kind: 'Module', bindings};
+        },
+    }));
+
+    // STEP 2: Resolve all Identifier nodes
+    for (let [id, scope] of identifiers) {
+        const {uniqueName} = scope.lookup(id.name);
+        Object.assign(id, {name: uniqueName, unique: true}); // TODO: messy overwrite of readonly prop - better/cleaner way?
+    }
+
+    // STEP 3: Resolve MemberExpression nodes where possible
+    for (let mem of memberExprs) {
+        let lhs = mem.module;
+        while (lhs.kind === 'Identifier' && lhs.unique && !lhs.placeholder) lhs = allSymbols[lhs.name].value; // TODO: could this loop infinitely?
+        assert(lhs.kind !== 'MemberExpression'); // TODO: explain... Since nested MemExprs are always resolved before outer ones due to them being added to the array depth-first
+
+        // If the lhs is a module, we can statically resolve the member expression. Otherwise, leave it as-is.
+        if (lhs.kind === 'Module') {
+            // Lookup the name in the lhs Module. This lookup is different to an Identifier lookup, in that the name
+            // must be local in the lhs Module, whereas Identifier lookups also look through the outer scope chain.
+            const id = lhs.bindings[mem.member];
+            if (!id) throw new Error(`'${mem.member}' is not defined`); // TODO: improve diagnostic message eg line+col
+            assert(id.kind === 'Identifier' && id.unique && !id.placeholder);
+            Object.assign(mem, {module: null, member: null}, id); // TODO: messy overwrite of readonly prop - better/cleaner way?
+        }
+    }
 
     // TODO: temp testing...
-    assert(resolved.kind === 'Identifier' && resolved.unique);
-    allSymbols['start'] = {uniqueName: 'start', value: resolved, scope: rootScope};
+    assert(startᐟ.kind === 'Identifier' && startᐟ.unique);
+    allSymbols['start'] = {uniqueName: 'start', value: startᐟ, scope: rootScope};
     const astᐟ: V.AST<300> = {
         version: 300,
         start: {
@@ -49,114 +134,6 @@ export function resolveSymbols(ast: V.AST<200>): V.AST<300> {
     };
     validateAST(astᐟ);
     return astᐟ;
-
-    // TODO: temp testing...
-    function internalResolve({gen, arg, env}: {gen: V.GenericExpression<200>, arg: V.Expression<200>, env: Scope}) {
-
-        // TODO: step 0 - synthesize a LetExpression
-        const top: V.LetExpression<200> = {
-            kind: 'LetExpression',
-            expression: gen.body,
-            bindings: {[gen.param]: arg},
-        };
-
-        // STEP 1: Traverse the AST, creating a scope for each module, and a symbol for each binding name/value pair.
-        const identifiers = new Map<V.Identifier, Scope>();
-        const memberExprs = [] as V.MemberExpression<300>[];
-        const result = mapNode(top, rec => ({
-            GenericExpression: ({param, body}): V.GenericExpression<300> => {
-
-                // Create a closure and nested scope for this generic expression.
-                const surroundingClosureId = currentClosureId;
-                currentClosureId = 'C' + String(++closureCounter).padStart(6, '0');
-                env = env.createNestedScope();
-
-                // Create a symbol for the generic parameter, whose value is specially marked as a 'placeholder'.
-                let placeholder: V.Identifier = {kind: 'Identifier', name: '', placeholder: true};
-                const {uniqueName} = env.insert(param, placeholder);
-                Object.assign(placeholder, {name: uniqueName}); // TODO: cleaner way than in-place update?
-
-                // Traverse the body expression in the new scope, then revert to the surrounding scope before returning.
-                const bodyᐟ = rec(body);
-                env = env.surroundingScope;
-                currentClosureId = surroundingClosureId;
-                return {kind: 'GenericExpression', param, body: bodyᐟ};
-            },
-            Identifier: id => {
-                if (id.unique) return id;
-                // TODO: explain tracking...
-                // Collect every Identifier encountered during the traversal, to be resolved in step 2.
-                const idᐟ = {...id};
-                identifiers.set(idᐟ, env);
-                return idᐟ;
-            },
-            LetExpression: le => {
-                // Create a nested scope for this let expression.
-                env = env.createNestedScope();
-
-                // Create a symbol for each local name in the let expression.
-                const bindings = {} as Record<string, V.Identifier>;
-                for (const [name, expr] of Object.entries(le.bindings)) {
-                    const {uniqueName} = env.insert(name, rec(expr));
-                    bindings[name] = {kind: 'Identifier', name: uniqueName, unique: true};
-                }
-
-                // Recursively resolve the main expression in the nested scope.
-                const expression = rec(le.expression);
-
-                // Pop back out to the surrounding scope before returning.
-                env = env.surroundingScope;
-                return expression;
-            },
-            MemberExpression: mem => {
-                // TODO: explain tracking...
-                // Collect every MemberExpression encountered during the traversal, to be resolved in step 3.
-                const memᐟ = {...mem, module: rec(mem.module)};
-                memberExprs.push(memᐟ);
-                return memᐟ;
-            },
-            Module: module => {
-                // Create a nested scope for this module.
-                env = env.createNestedScope();
-
-                // Create a symbol for each local name in the module.
-                const bindings = {} as Record<string, V.Identifier>;
-                for (const [name, expr] of Object.entries(module.bindings)) {
-                    const {uniqueName} = env.insert(name, rec(expr));
-                    bindings[name] = {kind: 'Identifier', name: uniqueName, unique: true};
-                }
-
-                // Pop back out to the surrounding scope before returning.
-                env = env.surroundingScope;
-                return {kind: 'Module', bindings};
-            },
-        }));
-
-        // STEP 2: Resolve all Identifier nodes
-        for (let [id, scope] of identifiers) {
-            const {uniqueName} = scope.lookup(id.name);
-            Object.assign(id, {name: uniqueName, unique: true}); // TODO: messy overwrite of readonly prop - better/cleaner way?
-        }
-
-        // STEP 3: Resolve MemberExpression nodes where possible
-        for (let mem of memberExprs) {
-            let lhs = mem.module;
-            while (lhs.kind === 'Identifier' && lhs.unique && !lhs.placeholder) lhs = allSymbols[lhs.name].value; // TODO: could this loop infinitely?
-            assert(lhs.kind !== 'MemberExpression'); // TODO: explain... Since nested MemExprs are always resolved before outer ones due to them being added to the array depth-first
-
-            // If the lhs is a module, we can statically resolve the member expression. Otherwise, leave it as-is.
-            if (lhs.kind === 'Module') {
-                // Lookup the name in the lhs Module. This lookup is different to an Identifier lookup, in that the name
-                // must be local in the lhs Module, whereas Identifier lookups also look through the outer scope chain.
-                const id = lhs.bindings[mem.member];
-                if (!id) throw new Error(`'${mem.member}' is not defined`); // TODO: improve diagnostic message eg line+col
-                assert(id.kind === 'Identifier' && id.unique && !id.placeholder);
-                Object.assign(mem, {module: null, member: null}, id); // TODO: messy overwrite of readonly prop - better/cleaner way?
-            }
-        }
-
-        return result;
-    }
 }
 
 
