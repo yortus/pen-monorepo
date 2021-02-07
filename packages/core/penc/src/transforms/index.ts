f7fc4b5f7caec74032a2f16ca89af831197c25a3
Troy Gerwien <yortus@gmail.com>
2021-02-07 16:56:43 +0800

    temp rename flatten transform dir

--- packages/core/penc/src/transforms/index.ts
+++ packages/core/penc/src/transforms/index.ts
@@ -1,6 +1,6 @@
 export {parseSourceFiles} from './01-parse-source-files';
 export {resolveSymbols} from './02-resolve-symbols';
-export {flattenExpressions} from './03-flatten-expressions';
+export {flattenExpressions} from './02b-flatten-expressions';
 export {normaliseExpressions} from './03-normalise-expressions';
 export {resolveConstantValues} from './04-resolve-constant-values';
 export {generateTargetCode} from './05-generate-target-code';
